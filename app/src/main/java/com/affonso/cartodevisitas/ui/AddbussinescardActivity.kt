d0c968199046d72570966e8d4153ef710a20f8a8
Pedro Henrique Garcia Affonso <pedro.affonso@fatec.sp.gov.br>
2022-07-01 18:00:40 -0300

    screen creation to add a business card

--- app/src/main/java/com/affonso/cartodevisitas/ui/AddbussinescardActivity.kt
+++ app/src/main/java/com/affonso/cartodevisitas/ui/AddbussinescardActivity.kt
@@ -1,24 +1,22 @@
 package com.affonso.cartodevisitas.ui
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.affonso.cartodevisitas.databinding.ActivityAddbussinescardBinding
 import com.affonso.cartodevisitas.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class AddbussinescardActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityAddbussinescardBinding.inflate(layoutInflater)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         insertListeners()
     }
-
     private fun insertListeners() {
-        binding.add.setOnClickListener {
-            val intent = Intent(this, AddbussinescardActivity::class.java)
-            startActivity(intent)
+        binding.btnClose.setOnClickListener {
+            finish()
         }
     }
 }
